081579066e747e353f8d579521e2a9da0a34ee85
Maksymilian Łakomy <github0@maksymilianlakomy.pl>
2022-11-12 12:43:55 +0100

    ci: update nexelem project

--- applications/nexelem/src/app/app.component.ts
+++ applications/nexelem/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'nexelem';
+  title = 'nexelem but with new title to check affected CI';
 }
