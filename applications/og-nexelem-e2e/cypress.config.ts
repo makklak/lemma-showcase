30f6e42744009e57037515a540d1efba4c627464
Maksymilian Łakomy <github0@maksymilianlakomy.pl>
2022-11-12 16:42:57 +0100

    feat: setup cucumber with cypress

--- applications/og-nexelem-e2e/cypress.config.ts
+++ applications/og-nexelem-e2e/cypress.config.ts
@@ -1,9 +1,49 @@
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+import * as webpack from '@cypress/webpack-preprocessor';
 import { defineConfig } from 'cypress';
-import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
 
 export default defineConfig({
   e2e: {
-    ...nxE2EPreset(__dirname),
     baseUrl: 'http://dev.lemma.virtuslab.com/',
+    specPattern: '**/*.feature',
+    supportFile: 'src/support/e2e.ts',
+    async setupNodeEvents(on, config): Promise<Cypress.PluginConfigOptions> {
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        'file:preprocessor',
+        webpack({
+          webpackOptions: {
+            resolve: {
+              extensions: ['.ts', '.js'],
+            },
+            module: {
+              rules: [
+                {
+                  test: /\.ts$/,
+                  exclude: [/node_modules/],
+                  use: [
+                    {
+                      loader: 'ts-loader',
+                    },
+                  ],
+                },
+                {
+                  test: /\.feature$/,
+                  use: [
+                    {
+                      loader: '@badeball/cypress-cucumber-preprocessor/webpack',
+                      options: config,
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+        })
+      );
+
+      return config;
+    },
   },
 });
